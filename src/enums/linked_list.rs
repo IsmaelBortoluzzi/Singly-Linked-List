2ce8994cfd157c804c42128615ebf841dbe9e80b
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-05-27 11:25:36 -0300

    Implementada função pop() e push()

--- src/enums/linked_list.rs
+++ src/enums/linked_list.rs
@@ -1,5 +1,5 @@
 pub struct LinkedList {
-    pub head: Link,
+    pub head: Link, // Link = Option<Box<Node>>;
 }
 
 impl LinkedList {
@@ -9,25 +9,86 @@ impl LinkedList {
         }
     }
 
-    pub fn push(&mut self, element: i32) {
-        match self.head {
+    pub fn push(&mut self, new_element: i32) {
+
+        let old_head = std::mem::replace(&mut self.head, None);
+
+        match old_head {
             None => { 
                 self.head = Some(Box::new(Node {
-                    element: element,
+                    element: new_element,
                     next: None,
-                }))
+                }));
             }
 
-            Some(n: Box<Node>) => {
+            Some(n) => {
                 
                 let new_head = Some(Box::new(Node {
-                    element: element,
-                    next: n,
+                    element: new_element,
+                    next: Some(n),
                 }));
 
                 self.head = new_head;
             }
         }
+
+
+        // O jeito melhor seria:
+
+        /* 
+        
+        let old_head = self.head.take()
+
+         let new_head = Box::new(Node {
+             element: new_element,
+             next: old_head,
+         });
+
+         self.head = Some(new_head);
+        
+        */
+
+    }
+
+    // fn append(&mut self, new_element: i32) {
+    //     match self.head {
+    //         None => {
+    //             self.head = Some(Box::new(Node {
+    //                 element: new_element,
+    //                 next: None,
+    //             }));
+    //         }
+
+    //         Some(n) => {
+    //             let mut aux = self.head.as_deref().unwrap().next.as_deref().unwrap();  // we already know head is not None
+    //             while self.head.as_deref().unwrap().next.is_some() {
+    //                 aux = aux.next.as_deref().unwrap();
+    //             }
+    //         }
+    //     }
+    // }
+
+
+    pub fn pop_head(&mut self) -> Option<i32> {
+        let old_head = self.head.take();
+
+        match old_head {
+            None => { 
+                None 
+            }
+            
+            Some(n) => {
+                self.head = n.next;
+                Some(n.element)
+            }
+        }
+
+        // O jeito melhor
+        
+        // self.head.take().map(|n|, {
+        //     self.head = n.next;
+        //     n.element
+        // })
     }
 
 }
@@ -36,9 +97,9 @@ impl LinkedList {
 
 // PRIVATE
 
-type Link = Option<Box<Node>>;  // Tipo o typedef
+pub type Link = Option<Box<Node>>;  // Tipo o typedef
 
-struct Node {
-    element: i32, 
-    next: Link,
+pub struct Node {
+    pub element: i32, 
+    pub next: Link,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use linked_list::*;
 
 fn it_works() {
     
-    let list: LinkedList = LinkedList::new_empty_sll();
+    let mut list: LinkedList = LinkedList::new_empty_sll();
+    list.push(10);
+    list.push(20);
 
-    println!("{}", list.element);
+    println!("{}", list.head.as_deref().unwrap().element);
+    println!("{}", list.head.as_deref().unwrap().next.as_deref().unwrap().element);
+    list.pop_head();
+    println!("{}", list.head.as_deref().unwrap().element);
+    println!("{}", list.head.as_deref().unwrap().next.as_deref().unwrap().element);
 
 }
 
